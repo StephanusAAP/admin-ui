34a93d81edf984ecfa92062e6184793281c55076
StephanusAAP <111202314998@mhs.dinus.ac.id>
2026-04-28 15:03:29 +0700

    UTS

--- src/Exercise.jsx
+++ src/Exercise.jsx
@@ -5,7 +5,7 @@ import { posts } from "./postsData";
 function Exercise() {
   return (
     <div className="min-h-screen bg-gray-100 p-6">
-      <h1 className="text-3xl font-bold text-center mb-6 text-blue-700">
+      <h1 className="text-3xl font-bold text-center mb-6 text-red-700">
         Post Cards
       </h1>
 
