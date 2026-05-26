193d6196fb70fee2fee72201be9dc108913abc64
StephanusAAP <111202314998@mhs.dinus.ac.id>
2026-05-26 08:50:57 +0700

    atomic design page sign in dan sign up

---
+++ src/components/Elements/CheckBox.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Checkbox(props) {
+  const { label, id, ...rest } = props;
+
+  return (
+    <>
+      <input className="text-sm accent-primary" id={id} {...rest} />
+      <label htmlFor={id} className="text-sm text-gray-01 m1-6">
+        {label}
+      </label>
+    </>
+  );
+}
+
+export default Checkbox;
