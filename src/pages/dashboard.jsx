c322bab53bbe1c6455d02d40032612646f6f3a08
StephanusAAP <111202314998@mhs.dinus.ac.id>
2026-05-28 10:28:33 +0700

    State in Component with Hooks

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,51 +1,40 @@
 import React from "react";
 import MainLayout from "../components/Layouts/MainLayout";
-import Card from "../components/Elements/Card";
+import CardBalance from "../components/Fragments/CardBalance";
+import CardGoal from "../components/Fragments/CardGoal";
+import CardUpcomingBill from "../components/Fragments/CardUpcomingBill";
+import CardRecentTransaction from "../components/Fragments/CardRecentTransaction";
+import CardStatistic from "../components/Fragments/CardStatistic";
+import CardExpensesBreakdown from "../components/Fragments/CardExpensesBreakdown";
+import { transactions, bills, expensesBreakdowns } from "../data";
 
 function Dashboard() {
+  console.log(transactions);
   return (
-    <MainLayout>
-      <div className="grid sm:grid-cols-12 sm:grid-rows-3 gap-6 h-full">
-        <div className="sm:col-span-4">
-          <Card
-            title="Total Balance"
-            desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, autem porro asperiores numquam sed veritatis debitis beatae amet laboriosam fuga pariatur sapiente suscipit culpa facere voluptatem. Repellat asperiores doloribus earum!"
-          />
+    <>
+      <MainLayout>
+        <div className="grid sm:grid-cols-12 gap-6">
+          <div className="sm:col-span-4">
+            <CardBalance />
+          </div>
+          <div className="sm:col-span-4">
+            <CardGoal />
+          </div>
+          <div className="sm:col-span-4">
+            <CardUpcomingBill data={bills} />
+          </div>
+          <div className="sm:col-span-4 sm:row-span-2">
+            <CardRecentTransaction data={transactions} />
+          </div>
+          <div className="sm:col-span-8">
+            <CardStatistic />
+          </div>
+          <div className="sm:col-span-8">
+            <CardExpensesBreakdown data={expensesBreakdowns} />
+          </div>
         </div>
-        <div className="sm:col-span-4">
-          <Card
-            title="Goals"
-            desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, autem porro asperiores numquam sed veritatis debitis beatae amet laboriosam fuga pariatur sapiente suscipit culpa facere voluptatem. Repellat asperiores doloribus earum!"
-          />
-        </div>
-        <div className="sm:col-span-4">
-          <Card
-            title="Upcoming Bill"
-            link="/bill"
-            desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, autem porro asperiores numquam sed veritatis debitis beatae amet laboriosam fuga pariatur sapiente suscipit culpa facere voluptatem. Repellat asperiores doloribus earum!"
-          />
-        </div>
-        <div className="sm:col-span-4 sm:row-span-2">
-          <Card
-            title="Recent Transactions"
-            link="/transactions"
-            desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, autem porro asperiores numquam sed veritatis debitis beatae amet laboriosam fuga pariatur sapiente suscipit culpa facere voluptatem. Repellat asperiores doloribus earum!"
-          />
-        </div>
-        <div className="sm:col-span-8">
-          <Card
-            title="Statistics"
-            desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, autem porro asperiores numquam sed veritatis debitis beatae amet laboriosam fuga pariatur sapiente suscipit culpa facere voluptatem. Repellat asperiores doloribus earum!"
-          />
-        </div>
-        <div className="sm:col-span-8">
-          <Card
-            title="Expenses Breakdown"
-            desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, autem porro asperiores numquam sed veritatis debitis beatae amet laboriosam fuga pariatur sapiente suscipit culpa facere voluptatem. Repellat asperiores doloribus earum!"
-          />
-        </div>
-      </div>
-    </MainLayout>
+      </MainLayout>
+    </>
   );
 }
 
